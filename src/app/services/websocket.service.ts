bbb679a856b4c900494557deba6d46d6dfcde452
tcsangulartraining <tcsangulartraining@gmail.com>
2022-03-23 18:03:31 +0530

    2 files modified

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import * as io from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { Observable, of, throwError } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebsocketService {
-  private socket: SocketIOClient.Socket;
-  constructor() {}
+  private socket: Socket;
+  constructor() {
+    this.socket = io.connect(environment.apiUrl);
+  }
 }
